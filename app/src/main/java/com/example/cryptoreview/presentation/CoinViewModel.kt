1d76c13a0219b13fe7c8b9120c9cb4320965870f
Glebasta33 <gleb.trusow@yandex.ru>
2021-11-26 15:16:21 +0300

    add RefreshDataWorker.kt

--- app/src/main/java/com/example/cryptoreview/presentation/CoinViewModel.kt
+++ app/src/main/java/com/example/cryptoreview/presentation/CoinViewModel.kt
@@ -1,22 +1,13 @@
 package com.example.cryptoreview.presentation
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.cryptoreview.data.network.ApiFactory
-import com.example.cryptoreview.data.database.AppDatabase
-import com.example.cryptoreview.data.network.model.CoinInfoDto
-import com.example.cryptoreview.data.network.model.CoinInfoJsonContainerDto
 import com.example.cryptoreview.data.repository.CoinRepositoryImpl
 import com.example.cryptoreview.domain.GetCoinInfoListUseCase
 import com.example.cryptoreview.domain.GetCoinInfoUseCase
 import com.example.cryptoreview.domain.LoadDataUseCase
-import com.google.gson.Gson
 import kotlinx.coroutines.launch
-import java.util.concurrent.TimeUnit
 
 class CoinViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -31,9 +22,7 @@ class CoinViewModel(application: Application) : AndroidViewModel(application) {
     fun getDetailInfo(fSym: String) = getCoinInfoUseCase(fSym)
 
     init {
-        viewModelScope.launch {
-            loadDataUseCase()
-        }
+        loadDataUseCase()
     }
 
 
