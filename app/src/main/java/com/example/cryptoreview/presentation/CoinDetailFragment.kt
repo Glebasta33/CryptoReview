757e35dba6ea634c060e69d28955d3270f3152ea
Glebasta33 <gleb.trusow@yandex.ru>
2021-11-26 14:00:12 +0300

    add generated code

--- app/src/main/java/com/example/cryptoreview/presentation/CoinDetailFragment.kt
+++ app/src/main/java/com/example/cryptoreview/presentation/CoinDetailFragment.kt
@@ -21,7 +21,7 @@ class CoinDetailFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentCoinDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
